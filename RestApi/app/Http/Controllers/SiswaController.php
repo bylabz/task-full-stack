9b5a932f8c3a1fd56b56a6246cddbcbe38d76a4b
mkhafid <muhammad.khafid@hostinger.com>
2019-11-20 18:02:28 +0700

    The result of task

---
+++ RestApi/app/Http/Controllers/SiswaController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Siswa;
+
+class SiswaController extends Controller
+{
+    //
+
+    public function index(){
+
+    	$siswa=Siswa::all();
+
+    	$data=['siswa'=>$siswa];
+
+    	return $data;
+    }
+
+    public function create(Request $request){
+
+    	$siswa=new Siswa();
+    	$siswa->nama=$request->nama;
+    	$siswa->alamat=$request->alamat;
+    	$siswa->save();
+
+    	return " Data Tersimpan";
+    }
+
+    public function update(Request $request, $id){
+
+    	$siswa=Siswa::find($id);
+    	$siswa->nama=$request->nama;
+    	$siswa->alamat=$request->alamat;
+    	$siswa->save();
+
+    	return " Data Terupdate";
+    }
+
+    public function delete($id){
+
+    	$siswa=Siswa::find($id);
+    	$siswa->delete();
+
+    	return " Data Terhapus";
+    }
+
+    public function detail($id){
+
+    	$siswa=Siswa::find($id);
+
+    	return $siswa;
+    }
+
+}
